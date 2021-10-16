e2c03e183daa984a92cb0b6fd88072269370fa70
BaptiseLrt <62546189+BaptiseLrt@users.noreply.github.com>
2021-10-16 10:35:22 +0200

    Initial commit

---
+++ vehicloud/vehicloud.ino
@@ -0,0 +1,18 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <dummy.h>
+
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
