a80b731c8fefe2a0f6827c8253902b07142d21ad
BaptiseLrt <62546189+BaptiseLrt@users.noreply.github.com>
2021-11-12 19:23:42 +0100

    V02 compiling, first prototype with sampled datas

---
+++ vehicloud_esp82_V02/config.h
@@ -0,0 +1,16 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+const char* ssid="Cognock";
+const char* wifipass= "lacognockestrondepourceuxquilaiment";
+
+
+const String serverName = "http://192.168.1.128:8282/~/mn-cse/mn-name/";
+
+// information in con
+const String Category =  "PPM, Luminosité et Humidité";
+const String Location = "43.570793, 1.466292";
+
+
+
+#endif
