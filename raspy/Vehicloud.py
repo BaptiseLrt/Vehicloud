44b27dbac6df80509dccc6820cca2cbae4f33a42
DJZier <flomarkspitz_64@hotmail.fr>
2022-01-19 15:35:18 +0000

    Version finale sans scan et sans boucle infinie

--- raspy/Vehicloud.py
+++ raspy/Vehicloud.py
@@ -5,13 +5,31 @@ import requests
 import json
 import os
 import datetime
+import subprocess
+import time
 
 #Initialize the variables
 receivingData = False
 EOF = ['00','ff','00','ff','00','ff','00','ff','00','ff','00','ff','00','ff','00','ff','00','ff','00','ff','']
 myDate = datetime.date.today()
+listDevicesPath = r"list_devices.txt"
+#while(1): // Bloucle infinie qui ne march epas avec le sleep je comprends pas pourquoi
+#if (os.path.isfile(listDevicesPath)):
+#    os.remove(listDevicesPath)
+#os.system("source scan_ble.sh")  //first try to run the script
+#rc = subprocess.call(['sh', "source scan_ble.sh"]) //second try to run the script
+#output = os.popen("./scan_ble.sh") //third try
+listDevices = open(listDevicesPath, 'r')
 
-#child1 = pexpect.spawn("./scan_ble.sh")
+if (os.path.isfile('listBike.txt')):
+    os.remove('listBike.txt')
+bikeFile = open('listBike.txt', 'a')
+for lines in listDevices:
+    if (lines.find("Vehicloud")!= -1):
+        print("Found Bike !")
+        lines = lines.split(" ")
+        bikeFile.write(lines[0])
+bikeFile.close()
 
 # Run gatttool interactively.
 print("Running gatttool...")
@@ -46,28 +64,26 @@ for bike in bikeFile:
     	data = data.split(" ")
     	if(data == EOF):
     	    receiveData = False
-    	else:		    
-        	bikeId = int((data[18]+data[19]),16)
-        	time = str(myDate.year) + "-" + "0"+str(myDate.month) + "-" +str(myDate.day) + "T" + str(int((data[4]+data[5]),16)+1) + ":" + str(int((data[6]+data[7]),16))+":00Z"
-        	print(data)
-        	location_lat = str(int((data[0]+data[1]),16)/1000)
-        	location_lon = str(int((data[2]+data[3]),16)/1000)
-        	temperature = str(int((data[8]+ data[9]),16))
-        	humidity = str(int((data[10]+ data[11]),16))
-        	gaz1 = str(int((data[12]+ data[13]),16)/100)
-        	gaz2 = str(int((data[14]+ data[15]),16)/100)
-        	print(bikeId),print(time),print(location_lat),print(location_lon),print(temperature),
-        	print(humidity),print(gaz1),print(gaz2)
-        	json_file = {"bikeId":bikeId, "time": time, "location_lat" : location_lat, "location_lon":location_lon, 
-        	"temperature": temperature, "humidity" : humidity, "gaz1": gaz1, "gaz2": gaz2}
-        	print("data = "),
-        	print(data)
-        	print("json = "),
-        	print(json_file)
-        	response = requests.post('http://vehicloud.herokuapp.com/sensorDatas/',json = json_file )
-        	print(response.text)
-        	print(response)
-        	print(".")
+    	else:
+    	    bikeId = int((data[18]+data[19]),16)
+    	    time = str(myDate.year) + "-" + "0"+str(myDate.month) + "-" +str(myDate.day) + "T" + str(int((data[4]+data[5]),16)+1) + ":" + str(int((data[6]+data[7]),16))+":00Z"
+    	    location_lat = str(int((data[0]+data[1]),16)/1000)
+    	    location_lon = str(int((data[2]+data[3]),16)/1000)
+    	    temperature = str(int((data[8]+data[9]),16))
+    	    humidity = str(int((data[10]+data[11]),16))
+    	    gaz1 = str(int((data[12]+data[13]),16)/100)
+    	    gaz2 = str(int((data[14]+data[15]),16)/100)
+    	    print(bikeId),print(time),print(location_lat),print(location_lon),print(temperature),
+    	    print(humidity),print(gaz1),print(gaz2)
+    	    json_file = {"bikeId":bikeId, "time": time, "location_lat" : location_lat, "location_lon":location_lon, "temperature": temperature, "humidity" : humidity, "gaz1": gaz1, "gaz2": gaz2}
+    	    print("data = "),
+    	    print(data)
+    	    print("json = "),
+    	    print(json_file)
+    	    response = requests.post('http://vehicloud.herokuapp.com/sensorDatas/',json = json_file )
+    	    print(response.text)
+    	    print(response)
+    	    print(".")
     	
 
 print("Data received")
